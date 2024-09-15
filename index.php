ec93ea0be1b3291717439c1d615afcb2da9b58fc
Arbit02 <167581541+Arbit02@users.noreply.github.com>
2024-09-15 16:38:39 +0300

    Add files via upload
    Пример использования
    $inn = 'Ваш ИНН';
    $result = GetContent($inn);
    echo $result;
    
    Возможные результаты:
    1 - является плательщиком налога на профессиональный доход
    0 - не является плательщиком налога на профессиональный доход

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+function GetContent($inn)
+{
+    $url = 'https://statusnpd.nalog.ru/api/v1/tracker/taxpayer_status';
+    $data = array(
+        'inn' => $inn,
+        'requestDate' => date('Y-m-d')
+    );
+    $data_json = json_encode($data);
+
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_POST, true);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $data_json);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, array(
+        'Content-Type: application/json',
+        'Content-Length: ' . strlen($data_json)
+    ));
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+    $responseData = json_decode($response, true);
+
+    if ($responseData['status']) {
+        return 1;
+    } else {
+        return 0;
+    }
+}
+
+$inn = '780204893183';
+$result = GetContent($inn);
+echo $result;
